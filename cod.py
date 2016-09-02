3419df3352ebdd4ffa4b48f0087ef51a4cd6e3e8
Adorilson Bezerra <adorilson@ppgsc.ufrn.br>
2016-09-01 23:18:46 -0300

    Getting downloads number from Google Play

--- cod.py
+++ cod.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import bs4 as bs
+import urllib
+from urllib import request
 
 #abre o arquivo
 xml = open('index.xml')
@@ -8,13 +10,26 @@ xml = open('index.xml')
 #instancia
 soup  = bs.BeautifulSoup(xml, 'xml')
 
-print "app | sdkever | targetsdk"
+URL_PLAY = "http://play.google.com/store/apps/details?id="
+
+print("app | sdkever | targetsdk | source | numDownloads")
 #loop para percorrer todas as app 
 for app in soup.findAll('application'):
     #pega o id da app. sempre sera a primeira linha id e sdkver
     id = app.get('id')
-    source = app.source.string
-    #loop para pegar todos os packages
-    for pkg in app.findAll('package'):
-        targetSdk = pkg.targetSdkVersion.string if pkg.targetSdkVersion!=None else ""
-        print id, "|", pkg.sdkver.string, "|", targetSdk, "|", source
+    
+    url_app = URL_PLAY + id
+    
+    try:
+        r = request.urlopen(url_app)
+        soup_html = bs.BeautifulSoup(r.read(), 'html.parser')
+        num_downloads = soup_html.find_all("div", attrs={"itemprop":"numDownloads"}) 
+        # some app doesn't have numDownloads
+        if num_downloads:
+            source = app.source.string
+            #loop para pegar todos os packages
+            for pkg in app.findAll('package'):
+                targetSdk = pkg.targetSdkVersion.string if pkg.targetSdkVersion!=None else ""
+                print(id, "|", pkg.sdkver.string, "|", targetSdk, "|", source, "|", num_downloads[0].string)
+    except urllib.error.HTTPError as e:
+        pass
